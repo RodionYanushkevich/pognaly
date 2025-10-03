d7e31b4b4adecd41eeacd20d975df0a96e30b84b
Rodion Yanushkevich <rodion.yanushkevich@mail.ru>
2025-10-04 00:49:38 +0700

    feat: form

--- source/js/modules/noui-slider/noui-slider.js
+++ source/js/modules/noui-slider/noui-slider.js
@@ -3,21 +3,44 @@ import noUiSlider from '../../vendor/nouislider';
 
 const initNOUISlider = () => {
   const slider = document.querySelector('[data-slider="noui-slider"]');
+  const form = document.querySelector('[data-form]');
+  const minInput = document.getElementById('min-level');
+  const maxInput = document.getElementById('max-level');
 
+  const catalogButtons = document.querySelectorAll('[data-button="catalog-form-button"]');
   if (!slider) {
     return;
   }
 
-  noUiSlider.create(slider, {
-    start: [20, 80],
-    connect: true,
-    range: {
-      min: 0,
-      max: 100,
-    },
+  catalogButtons.forEach((button) => {
+    button.addEventListener('click', ()=>{
+      button.classList.toggle('catalog-form__button--is-active');
+
+    });
   });
 
+  noUiSlider.create(slider, {
+    start: [30, 80],
+    connect: true,
+    range: {min: 0, max: 100},
+  });
 
+  slider.noUiSlider.on('update', function (values) {
+    minInput.value = Math.round(values[0]);
+    maxInput.value = Math.round(values[1]);
+  });
+
+  minInput.addEventListener('change', function () {
+    slider.noUiSlider.set([minInput.value, null]);
+  });
+
+  maxInput.addEventListener('change', function () {
+    slider.noUiSlider.set([null, maxInput.value]);
+  });
+
+  form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+  });
 };
 
 export {initNOUISlider};
